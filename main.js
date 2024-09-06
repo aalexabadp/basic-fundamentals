c1dec22afed3342ca604336cea67197605e73164
aalexabadp <aalexabad96@outlook.com>
2024-09-06 01:57:25 -0500

    feat: tipos de datos: primitivos y tipo-objetos

--- main.js
+++ main.js
@@ -3,3 +3,37 @@ var perro = 'Bobby'
 
 const direction = 'avenida Paseos'
 console.log(direction);
+
+let velocidad = 98.5
+console.log(typeof velocidad)
+
+let userName = 'Sergio'
+console.log(typeof userName)
+
+let isActive = true
+console.log(typeof isActive)
+
+let nombre
+console.log(nombre)
+
+let edad = null
+console.log(edad)
+
+let z = 3n ** 55n
+console.log(typeof z)
+
+let clave = Symbol('Sym')
+console.log(typeof clave)
+
+let carros = ['Ford Mustang', 500, true]
+console.log(typeof (carros))
+
+let carro = { modelo: 'Ford Mustang', potencis: 300, velocidad: 200 }
+
+let x = [2, 4]
+let y = x
+
+y.push(5)
+
+console.log(x)
+console.log(y)
