c4d01601a7a45ce3aa8ecf8f6dca8131542aa07e
aalexabadp <aalex.abad96@outlook.com>
2024-09-08 17:25:40 -0500

    feat: ciclos

---
+++ ciclos.js
@@ -0,0 +1,28 @@
+/** CICLOS IMPLEMENTADOS CON FOR */
+
+// for(let i = 1 ; i <= 4; i++) {
+//   console.log(`iteracion ${i}`);
+// }
+
+// console.log('Fin del ciclo')
+
+/** CICLOS ANIDADOS */
+
+// for (let i = 1; i <= 4; i++){
+//   console.log(`Empezar iteración ${i}`)
+//   for (let j = 1; j <= 4; j++){
+//     console.log(j)
+//   }
+// }
+
+// console.log(`Fin de ciclo`)
+
+/** WHILE */
+
+let x = 2
+while (x <= 4) {
+  console.log(`Iteracion ${x}`)
+  x++
+}
+
+console.log(`Fin de ciclo`)
